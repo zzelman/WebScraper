6c64b1cf14d6119757d5b52287bb6f55f13a0a3b
zzelman <zzelman@localhost.localdomain>
2014-09-23 11:08:08 -0600

    Worked on extracting common code into a library and documentation of that library

--- 4chan/main/src/post_4chan.py
+++ 4chan/main/src/post_4chan.py
@@ -1,4 +1,13 @@
 class Post_4chan:
+
+	'''
+		Constructor for this class
+
+		ARGS:
+			reQuote: 	a list of strs that point to a previous post
+			imgLink: 	the link to the image that accompanies this post (if one exists)
+			postText: 	the text contained within this post
+	'''
 	def __init__(self, reQuote, imgLink, postText):
 		self.reQuote 	= reQuote
 		self.imgLink 	= imgLink
--- 4chan/main/src/thread_4chan.py
+++ 4chan/main/src/thread_4chan.py
@@ -1,4 +1,12 @@
 class Thread_4chan:
+	'''
+		Constructor for this Thread_4chan object
+
+		ARGS:
+			thread_url: 	The url that this thread occored at
+			read_time: 		str representation of the time that this thread was parsed at
+			thread_data: 	a list of Post_4chan objects
+	'''
 	def __init__(self, thread_url, read_time, thread_data):
 		self.thread_url 	= thread_url
 		self.read_time 		= read_time
